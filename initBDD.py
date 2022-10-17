b9e1f8d1c50f1e106018b42b50604f8635553755
peufgeul <98660368+peufgeul@users.noreply.github.com>
2022-10-17 11:20:00 +0200

    modification validation données

--- initBDD.py
+++ initBDD.py
@@ -1,4 +1,3 @@
-
 import sqlite3 as lite
 
 #créer la connexion avec le fichier, et créer le fichier si il est inexistant
@@ -16,46 +15,49 @@ print(listeTable)
 if "fournisseur" not in listeTable:
 	print("création de la table fournisseur")
 	cur.execute("""CREATE TABLE fournisseur(
-		id   SMALLINT UNSIGNED PRIMARY KEY NOT NULL,
+		id   INTEGER PRIMARY KEY AUTOINCREMENT ,
 		nom  TEXT)""")
 
 if "vehicule" not in listeTable:
 	print("création de la table vehicule")
 	cur.execute("""CREATE TABLE vehicule(
-		id   SMALLINT UNSIGNED PRIMARY KEY NOT NULL,
+		id   INTEGER PRIMARY KEY AUTOINCREMENT ,
 		nom  TEXT)""")
 
 if "pieces" not in listeTable:
 	print("création de la table pieces")
 	cur.execute("""CREATE TABLE pieces(
-		id SMALLINT UNSIGNED PRIMARY KEY NOT NULL,
-		designation         TEXT,
-		code_article        TEXT,
-		idFournisseur       SMALLINT UNSIGNED,
-		stock               INT,
+		id   INTEGER PRIMARY KEY AUTOINCREMENT ,
+		designation         TEXT NOT NULL,
+		code_article        TEXT NOT NULL,
+		idFournisseur       INTEGER UNSIGNED NOT NULL,
+		stock               INT DEFAULT 0,
 		seuil_commande      INT UNSIGNED,
 		delai               INT UNSIGNED,
-		niveau_recompletion INT UNSIGNED)""")
+		niveau_recompletion INT UNSIGNED,
+		FOREIGN KEY(idFournisseur) REFERENCES fournisseur(id))
+		""")
 
 if "kits" not in listeTable:
 	print("création de la table kits")
 	cur.execute("""CREATE TABLE kits(
-		id   SMALLINT UNSIGNED PRIMARY KEY NOT NULL,
+		id   INTEGER PRIMARY KEY AUTOINCREMENT ,
 		nom  TEXT)""")
 
 if "commande_vehicule" not in listeTable:
 	print("création de la table commande_vehicule")
 	cur.execute("""CREATE TABLE commande_vehicule(
-		id  SMALLINT UNSIGNED PRIMARY KEY NOT NULL,
-		id_vehicule      SMALLINT UNSIGNED NOT NULL,
+		id   INTEGER PRIMARY KEY AUTOINCREMENT ,
+		id_vehicule      INTEGER UNSIGNED NOT NULL,
 		date_commande    TIMESTAMP,
 		date_validation  TIMESTAMP,
-		etat             TEXT)""")
+		etat             TEXT,
+		FOREIGN KEY(id_vehicule) REFERENCES vehicule(id))""")
 
 if "commande_kits" not in listeTable:
 	print("création de la table commande_kits")
 	cur.execute("""CREATE TABLE commande_kits(
-		id  SMALLINT UNSIGNED PRIMARY KEY NOT NULL,
+		id   INTEGER PRIMARY KEY AUTOINCREMENT ,
 		date_commande    TIMESTAMP,
 		date_validation  TIMESTAMP,
 		etat             TEXT)""")
@@ -63,40 +65,49 @@ if "commande_kits" not in listeTable:
 if "commande_pieces" not in listeTable:
 	print("création de la table commande_pieces")
 	cur.execute("""CREATE TABLE commande_pieces(
-		id  SMALLINT UNSIGNED PRIMARY KEY NOT NULL,
+		id   INTEGER PRIMARY KEY AUTOINCREMENT ,
 		date_commande    TIMESTAMP,
 		date_validation  TIMESTAMP,
 		etat             TEXT,
-		idFournisseur    SMALLINT UNSIGNED)""")
+		idFournisseur    INTEGER UNSIGNED NOT NULL,
+		FOREIGN KEY(idFournisseur) REFERENCES fournisseur(id))""")
 		
 		
 
 if "piece_kit" not in listeTable:
 	print("création de la table piece_kit")	
 	cur.execute("""CREATE TABLE piece_kit(
-		id_piece      SMALLINT UNSIGNED NOT NULL,
-		id_kit        SMALLINT UNSIGNED NOT NULL,
-		nombre_piece  INT UNSIGNED NOT NULL)""")
+		id_piece      INTEGER UNSIGNED NOT NULL,
+		id_kit        INTEGER UNSIGNED NOT NULL,
+		nombre_piece  INT UNSIGNED NOT NULL,
+		FOREIGN KEY(id_piece) REFERENCES pieces(id),
+		FOREIGN KEY(id_kit)   REFERENCES kits(id))""")
 
 if "contenu_commande_kit" not in listeTable:
 	print("création de la table commande_kits")
 	cur.execute("""CREATE TABLE contenu_commande_kit(
-		id_kit       SMALLINT UNSIGNED NOT NULL,
-		id_commande  SMALLINT UNSIGNED NOT NULL,
-		nombre_kit   INT UNSIGNED NOT NULL)""")
+		id_kit       INTEGER UNSIGNED NOT NULL,
+		id_commande  INTEGER UNSIGNED NOT NULL,
+		nombre_kit   INT UNSIGNED NOT NULL,
+		FOREIGN KEY(id_kit)      REFERENCES kits(id),
+		FOREIGN KEY(id_commande) REFERENCES commande_kits(id))""")
 
 if "contenu_commande_pieces" not in listeTable:
 	print("création de la table contenu_commande_pieces")
 	cur.execute("""CREATE TABLE contenu_commande_pieces(
-		id_piece      SMALLINT UNSIGNED NOT NULL,
-		id_commande   SMALLINT UNSIGNED NOT NULL,
-		nombre_piece  INT UNSIGNED NOT NULL)""")
+		id_piece      INTEGER UNSIGNED NOT NULL,
+		id_commande   INTEGER UNSIGNED NOT NULL,
+		nombre_piece  INT UNSIGNED NOT NULL,
+		FOREIGN KEY(id_piece)    REFERENCES pieces(id),
+		FOREIGN KEY(id_commande) REFERENCES commande_pieces(id))""")
 
 if "kit_dans_vehicule" not in listeTable:
 	print("création de la table kit_dans_vehicule")
 	cur.execute("""CREATE TABLE kit_dans_vehicule(
-		id_kit       SMALLINT UNSIGNED NOT NULL,
-		id_vehicule  SMALLINT UNSIGNED NOT NULL,
-		nombre_kit   INT UNSIGNED NOT NULL)""")
+		id_kit       INTEGER UNSIGNED NOT NULL,
+		id_vehicule  INTEGER UNSIGNED NOT NULL,
+		nombre_kit   INT UNSIGNED NOT NULL,
+		FOREIGN KEY(id_kit)      REFERENCES kits(id),
+		FOREIGN KEY(id_vehicule) REFERENCES vehicule(id))""")
 
 con.close()
