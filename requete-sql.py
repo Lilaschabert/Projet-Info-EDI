2fddabc9f63cf78e0fc288e71a453ac94c8b53bb
D&non <115975068+Denon72-40@users.noreply.github.com>
2022-10-17 11:48:05 +0200

    Update requete-sql.py

--- requete-sql.py
+++ requete-sql.py
@@ -1,3 +1,6 @@
+## AGIGREEN + AGIPARTS ##
+
+**
 Page a expedier
 
 commande = SELECT id FROM commande_pieces
@@ -10,14 +13,23 @@ Page historique
 
 SELECT id, date_validation, etat FROM commande_pieces
 
+
+
+
+
+
+## AGILOG
+
+LIEN AVEC AGIGREEN/PARTS
+
 **
 Page réception
 
 commande = SELECT id FROM commande_pieces
 UPDATE commande_pieces SET etat=valide WHERE commande_pieces.id == commande
-UPDATE commande_pieces SET etat=invalide WHERE commande_pièces.id == commande
+UPDATE commande_pieces SET etat=invalide WHERE commande_pieces.id == commande
 
-UPDATE commande_pièces SET date validation=date WHERE commande_pieces.id == commande
+UPDATE commande_pièces SET date_validation=date WHERE commande_pieces.id == commande
 
 **
 Page stock initiaux
@@ -37,4 +49,42 @@ SELECT id, stock FROM pieces
 **
 Page passer commande
 
-INSERT INTO contenu_commande_pieces SET etat=valide WHERE commande_pieces.id == commande
+SELECT id, stock, seuil_commande, delai, niveau_recompletion FROM pieces
+
+"INSERT INTO contenu_commande_pieces('id_piece','id_commande','nombre_piece') VALUES (?,?,?)" (id_piece,id_commande,nombre_piece)
+"INSERT INTO commande_pieces('id','date_commande','date_validation','etat','idFournisseur') VALUES (?,?,?,?,?)" (id,date_commande,NONE,'en cours',idFournisseur)
+
+
+
+LIEN AVEC AGILEAN
+
+**
+Page a expedier
+
+commande = SELECT id FROM commande_kits
+
+quand envoi:
+UPDATE commande_kits SET etat=envoye WHERE commande_kits.id == commande
+
+
+
+
+
+
+## AGILEAN ##
+
+**
+Page passer commande
+
+SELECT nom FROM kits
+"INSERT INTO contenu_commande_kit('id_kit','id_commande','nombre_kit') VALUES (?,?,?)" (id_kit,id_commande,nombre_kit)
+"INSERT INTO commande_kit('id','date_commande','date_validation','etat') VALUES (?,?,?,?)" (id,date_commande,NONE,'en cours')
+
+**
+Page reception
+
+commande = SELECT id FROM commande_kits
+UPDATE commande_kits SET etat=valide WHERE commande_kits.id == commande
+UPDATE commande_kits SET etat=invalide WHERE commande_kits.id == commande
+
+UPDATE commande_kits SET date_validation=date WHERE commande_kits.id == commande
